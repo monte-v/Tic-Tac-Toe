4abf5fa833aecb804b8945b2733fa8b6a71b9b5b
monte-v <vdmitry08@gmail.com>
2024-10-19 21:06:40 +0300

    Сделал правила игры и закомитил

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
@@ -7,9 +9,25 @@ namespace Tic_Tac_Toe
             InitializeComponent();
         }
 
+        bool player = false;
+        int[] gameBoard = new int[9];
+
         private void textBox_Click(object sender, EventArgs e)
         {
+            TextBox clickedTextBox = (TextBox)sender;
 
+            // ќпредел€ем номер €чейки, соответствующей нажатому TextBox
+            int cellIndex = int.Parse(clickedTextBox.Name.Substring(7)) - 1;
+
+            // ≈сли €чейка пуста, то ставим значение игрока
+            if (gameBoard[cellIndex] == 0)
+            {
+                gameBoard[cellIndex] = player ? 1 : 2; // 1 - X, 2 - O
+                clickedTextBox.Text = player ? "X" : "O";
+                player = !player; // ѕереключаем хода
+            }
+
+            //WinnerCheck();
         }
 
         private void panel_Click(object sender, EventArgs e)
