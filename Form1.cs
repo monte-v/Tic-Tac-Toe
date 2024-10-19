7713bd2f8b283ae3e958e2dcdf18b9f352ea23f0
monte-v <vdmitry08@gmail.com>
2024-10-19 20:44:49 +0300

    Сделал дизайн приложения и закомитил

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,27 @@ namespace Tic_Tac_Toe
         {
             InitializeComponent();
         }
+
+        private void textBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            label4.ForeColor = Color.Black;
+            panel2.BackColor = Color.Lavender;
+        }
+
+        private void panel_MouseUp(object sender, MouseEventArgs e)
+        {
+            label4.ForeColor = Color.White;
+            panel2.BackColor = Color.SlateBlue;
+        }
     }
 }
