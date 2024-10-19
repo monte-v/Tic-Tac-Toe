d60e6ac6fea7978f19ee9b9aa0ddd71401258aca
monte-v <vdmitry08@gmail.com>
2024-10-19 22:00:59 +0300

    Сделал возможность выигрыша и закомитил

--- Form1.cs
+++ Form1.cs
@@ -11,6 +11,8 @@ namespace Tic_Tac_Toe
 
         bool player = false;
         int[] gameBoard = new int[9];
+        int playerXWins = 0;
+        int playerOWins = 0;
 
         private void textBox_Click(object sender, EventArgs e)
         {
@@ -25,9 +27,75 @@ namespace Tic_Tac_Toe
                 gameBoard[cellIndex] = player ? 1 : 2; // 1 - X, 2 - O
                 clickedTextBox.Text = player ? "X" : "O";
                 player = !player; // Переключаем хода
+                WinnerCheck();
             }
 
-            //WinnerCheck();
+
+        }
+        private void WinnerCheck()
+        {
+            // Проверка строк
+            for (int i = 0; i < 9; i += 3)
+            {
+                if (gameBoard[i] > 0 && gameBoard[i] == gameBoard[i + 1] && gameBoard[i] == gameBoard[i + 2])
+                {
+                    if (gameBoard[i] == 1)
+                        playerXWins++;
+                    else
+                        playerOWins++;
+                    ResetGame();
+                    return;
+                }
+            }
+
+            // Проверка столбцов
+            for (int i = 0; i < 3; i++)
+            {
+                if (gameBoard[i] > 0 && gameBoard[i] == gameBoard[i + 3] && gameBoard[i] == gameBoard[i + 6])
+                {
+                    if (gameBoard[i] == 1)
+                        playerXWins++;
+                    else
+                        playerOWins++;
+                    ResetGame();
+                    return;
+                }
+            }
+
+            // Проверка диагоналей
+            if (gameBoard[0] > 0 && gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8])
+            {
+                if (gameBoard[0] == 1)
+                    playerXWins++;
+                else
+                    playerOWins++;
+                ResetGame();
+                return;
+            }
+            if (gameBoard[2] > 0 && gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6])
+            {
+                if (gameBoard[2] == 1)
+                    playerXWins++;
+                else
+                    playerOWins++;
+                ResetGame();
+                return;
+            }
+
+        }
+        private void ResetGame()
+        {
+            // Сбрасываем состояние игрового поля
+            Array.Clear(gameBoard, 0, gameBoard.Length);
+
+            // Очищаем все TextBox
+            foreach (TextBox textBox in this.Controls.OfType<TextBox>())
+            {
+                textBox.Text = "";
+            }
+            // Отображаем счет игроков
+            label2.Text = $"X: {playerXWins}";
+            label3.Text = $"O: {playerOWins}";
         }
 
         private void panel_Click(object sender, EventArgs e)
